322334bf2db689ae3400b7d30bc38a0881eb0d09
michellesklee <michellesklee@gmail.com>
2020-12-01 10:51:36 -0700

    include comb_len as arg

--- code_solutions.py
+++ code_solutions.py
@@ -1,7 +1,7 @@
 import itertools
 import pandas as pd
 
-def day1_part1():
+def day1(comb_len):
     '''
     Based on an input of integers, find the combination of two integers where the sum equals 2020, then multiply the two together
     '''
@@ -9,16 +9,30 @@ def day1_part1():
         lines = f.readlines()
         lines = [int(x.strip()) for x in lines]
 
-    combinations = list(itertools.combinations(lines, 2))
+    def prod(val):
+        '''
+        Multiply elements in a tuple
+        '''
+        res = 1
+        for ele in val:
+            res *= ele
+        return res
+
+    combinations = list(itertools.combinations(lines, comb_len))
 
     combination_sums = []
     for comb in combinations:
         combination_sums.append(sum(comb))
 
-    combination_mult = [(x*y) for x, y in combinations]
+    combination_mult = []
+    for comb in combinations:
+        combination_mult.append(prod(comb))
 
-    comb_dict = {'combinations': combinations, 'sums': combination_sums, 'mult': combination_mult}
+    comb_dict = {'combination': combinations, 'sum': combination_sums, 'mult': combination_mult}
 
     comb_df = pd.DataFrame(comb_dict)
 
     print(comb_df.loc[comb_df['sums'] == 2020])
+
+if __name__ == '__main__':
+    day1(3)
